49ac09aee7558a3deef2a227ac51e78c586acbec
ZackeryJBrown <zackeryjbrown@gmail.com>
2022-11-26 07:56:28 -0600

    Index number fix

--- linearSearch.js
+++ linearSearch.js
@@ -1,14 +1,14 @@
 var testArray = [1,2,3,4,5,6,7,8,9,0];
 
 function linearSearch(inputArray, searchArg) {
-    for (let i=0; i < inputArray.length-1; i++){
+    for (let i=0; i < inputArray.length; i++){
         if (inputArray[i] === searchArg){
-            return inputArray[i];
+            return "Search Term Found at Index: " + i;
         };
     };
     return "Search term " + searchArg + " not found";
 };
 
-console.log(linearSearch(testArray, 4));
+console.log(linearSearch(testArray, 1));
 
 console.log(linearSearch(testArray, 10));
