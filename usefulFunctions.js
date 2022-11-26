339ff9c3b5dfcc87ffed371ad9dfee406343e72e
ZackeryJBrown <zackeryjbrown@gmail.com>
2022-11-26 10:10:53 -0600

    More function notes

--- usefulFunctions.js
+++ usefulFunctions.js
@@ -2,6 +2,14 @@
 somearray.push() //adds the contents in () to end of array, () can take strings or arrays
 .join() //returns array indexes as string, () contents will be between indexes, default comma
 .filter(function(currentValue, i, array){}) //returns a new array that pass tests provided by a user defined function inside .filter()
+.reduce((accumulator, currentVal) =>
+            {accumulator + currentVal.somePropertyOfObject;
+            return accumulator;
+            },0); /*starting accumulator value, ex 0*/
+
+
+someObject.every( (someObjects) => someObject.property === "someValue" ); //returns true or false based on if every instance of the checked item passing the testing function
+someObject.some( (someObjects) => someObject.property === "someValue" ); //returns true or false if 1 or more of the items pass the testing function
 
 //Stock Functions
 //---------------
